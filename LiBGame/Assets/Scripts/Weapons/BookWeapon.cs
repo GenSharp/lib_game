a14dddea0f28930072eece0166d831a559d81e96
GenSharp <lukamartan1@gmail.com>
2023-05-29 11:46:14 +0200

    Made the book weapon

--- LiBGame/Assets/Scripts/Weapons/BookWeapon.cs
+++ LiBGame/Assets/Scripts/Weapons/BookWeapon.cs
@@ -4,74 +4,46 @@ using UnityEngine;
 
 public class BookWeapon : MonoBehaviour
 {
-    public GameObject spherePrefab;
-    public float sphereRadius = 5f;
-    public float damageInterval = 1f;
-    public int damageAmount = 5;
-    public float manaDrainRate = 0.5f;
 
-    private bool isBookActive = false;
-    private float lastDamageTime = 0f;
-    private GameObject sphere;
+    public int minManaCost = 5;
+    public int maxManaCost = 15;
+    public int minDamage = 5;
+    public int maxDamage = 15;
+    public float fireRate = 3f;
+
+    private float nextFireTime;
+    private ManaSystem manaSystem;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        manaSystem = FindObjectOfType<ManaSystem>();
+        nextFireTime = 0f;
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
         {
-            ManaSystem manaSystem = FindObjectOfType<ManaSystem>();
-            if (!isBookActive && manaSystem.UseMana(1))
-            {
-                ActivateBook();
-            }
-            else
-            {
-                DeactivateBook();
-            }
-        }
+            int manaCost = Random.Range(minManaCost, maxManaCost + 1);
+            int damageAmount = Random.Range(minDamage, maxDamage + 1);
 
-        // Drain mana when book is active
-        if (isBookActive)
-        {
-            ManaSystem manaSystem = FindObjectOfType<ManaSystem>();
-            manaSystem.UseMana(Mathf.CeilToInt(manaDrainRate * Time.deltaTime));
-
-            if (manaSystem.currentMana <= 0)
+            if (manaSystem.UseMana(manaCost))
             {
-                DeactivateBook();
+                DealDamageToEnemies(damageAmount);
+                nextFireTime = Time.time + fireRate;
             }
         }
     }
 
-    void ActivateBook()
+    private void DealDamageToEnemies(int damageAmount)
     {
-        isBookActive = true;
-        sphere = Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        sphere.transform.localScale = new Vector3(sphereRadius, sphereRadius, sphereRadius);
-        sphere.GetComponent<Renderer>().enabled = true;
-    }
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
 
-    void DeactivateBook()
-    {
-        isBookActive = false;
-        Destroy(sphere);
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (isBookActive && other.CompareTag("Enemy") && Time.time - lastDamageTime > damageInterval)
+        foreach (Enemy enemy in enemies)
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, sphereRadius, LayerMask.GetMask("Enemy"));
-            foreach (Collider collider in colliders)
-            {
-                Enemy enemy = collider.GetComponent<Enemy>();
-                if (enemy != null)
-                {
-                    enemy.TakeDamage(damageAmount);
-                }
-            }
-
-            lastDamageTime = Time.time;
+            enemy.TakeDamage(damageAmount);
         }
     }
 }
