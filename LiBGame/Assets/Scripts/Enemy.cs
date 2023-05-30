36a4a2d6297017931f449c1ea54ca845828e0886
GenSharp <lukamartan1@gmail.com>
2023-05-30 21:11:50 +0200

    Fixed the hands weapon and added functionality to enemy
    Need to test out the enemy

--- LiBGame/Assets/Scripts/Enemy.cs
+++ LiBGame/Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -15,10 +16,24 @@ public class Enemy : MonoBehaviour
 
     private EnemyCounter enemyCounter;
 
+    private Animator animator;
+    private Transform target;
+    private NavMeshAgent agent;
+    public LayerMask isPlayer;
+    public float attackRange = 2f;
+    public bool playerInAttackRange;
+    private bool alreadyAttacked;
+    public float timeBetweenAttacks = 2f;
+    private float rotationSpeed = 10f;
+
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
         originalColor = meshRenderer.material.color;
+
+        target = GameObject.Find("Player").transform;
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Start is called before the first frame update
@@ -30,9 +45,19 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
+
+        if (playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+
+        else
+        {
+            ChasePlayer();
+        }
     }
 
     public void TakeDamage(int damageAmount)
@@ -50,8 +75,8 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         enemyCounter.EnemyKilled();
-
-        Destroy(gameObject);
+        animator.SetBool("Death", true);
+        Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
     }
 
     private IEnumerator DamageEffect()
@@ -59,5 +84,33 @@ public class Enemy : MonoBehaviour
         meshRenderer.material.color = damageColor;
         yield return new WaitForSeconds(colorChangeDuration);
         meshRenderer.material.color = originalColor;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void AttackPlayer()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if (!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+            HealthManager healthManager = target.GetComponent<HealthManager>();
+            if (healthManager != null)
+            {
+                healthManager.TakeDamage(10);
+            }
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 }
