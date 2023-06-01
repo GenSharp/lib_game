78e0d97cc88558022dee4c4e54435b5d41888e2c
GenSharp <lukamartan1@gmail.com>
2023-06-01 22:49:53 +0200

    Added things into the scene

--- LiBGame/Assets/Scripts/GameManager.cs
+++ LiBGame/Assets/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Scena1");
     }
 
     public void QuitGame()
